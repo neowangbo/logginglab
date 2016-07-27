049c85024e8f527055042cf6d0dab8b834d35278
Enjoy your code! <neo.wangbo@gmail.com>
2016-07-27 22:11:39 +0800

    create logging lab, and first project jcl

---
+++ jcl/src/main/java/com/martian/apps/logginglab/jcl/DemoFirst.java
@@ -0,0 +1,14 @@
+package com.martian.apps.logginglab.jcl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DemoFirst {
+
+	private final static Log LOG = LogFactory.getLog(DemoFirst.class);
+	
+	public static void main(String[] args){
+		
+	}
+	
+}
